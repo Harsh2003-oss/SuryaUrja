58b40d4c7de14e256c9bd417cd66007f9f84e5ab
Harsh2003-oss <harsh.sil872@gmail.com>
2026-06-22 21:56:12 +0530

    feat:added ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import SolarStory from "./components/SolarStory";
 
 export default function Home() {
   return (
     <main>
 <Navbar />
 <Hero />
+<SolarStory />
     </main>
   );
 }
